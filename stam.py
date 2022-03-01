9862d44ac069758109928b018efea9d29f118873
shady abu shaheen <shade.skill8@gmail.com>
2022-03-01 15:57:32 +0200

    trying new stuff

--- stam.py
+++ stam.py
@@ -1,9 +1,4 @@
 for x in range(10):
     print(x + 55)
-    print("hello world")
-    print("hello world")
-    print("hello world fxdx")
-    print("hello world fxdx")
-    print("hello world fxdx")
-    print("hello world fxdx")
+
 
